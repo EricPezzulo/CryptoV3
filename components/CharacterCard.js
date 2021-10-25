707b7277ba787433888a14ed094c31a4cea1b3b4
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-10-25 16:29:27 -0400

    switched data over to coingecko api

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function CharacterCard({ name, status, gender, image }) {
+function CharacterCard({
+  coinName,
+  coinSymbol,
+  coinID,
+  coinImage,
+  coinPriceChange24Hr,
+  coinMarketCap,
+  coinHigh24hr,
+  coinLow24Hr,
+}) {
   return (
-    <div className="w-64 bg-white rounded-lg ">
-      <h1>NAME:{name}</h1>
-      <p>Status:{status}</p>
-      <p>gender:{gender}</p>
-      <img className="rounded-b-lg" src={image}></img>
+    <div className="flex flex-col w-64 bg-white h-96 rounded-lg">
+      <div className="flex flex-col place-items-start pl-3 pt-2">
+        <h1>Name: {coinName}</h1>
+        <p>Symbol: {coinSymbol}</p>
+        <p>ID: {coinID}</p>
+        <p>Price Change %: {coinPriceChange24Hr}</p>
+        <p>Market Cap: {coinMarketCap}</p>
+        <p>24H High: {coinHigh24hr}</p>
+        <p>24H Low: {coinLow24Hr}</p>
+      </div>
+
+      <div className="flex w-full justify-center mt-4">
+        <img
+          className=" flex w-44 h-44 rounded-b-lg object-contain"
+          src={coinImage}
+        ></img>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,16 @@ import { useEffect, useState } from "react";
 import CharacterCard from "../components/CharacterCard";
 
 export default function Home() {
-  const [name, setName] = useState();
-  const [gender, setGender] = useState();
-  const [status, setStatus] = useState();
-  const [image, setImage] = useState();
-  const [characterData, setCharacterData] = useState([]);
-  const APIendpoint = "https://rickandmortyapi.com/api/character";
+  const [coinData, setCoinData] = useState([]);
+
+  const coinEndpoint =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
   useEffect(async () => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(APIendpoint);
-        setCharacterData(res.data.results);
-        setStatus(res.data.results[0].status);
-        setName(res.data.results[0].name);
-        setGender(res.data.results[0].gender);
-        setImage(res.data.results[0].image);
+        const res = await axios.get(coinEndpoint);
+        setCoinData(res.data);
       } catch (error) {
         console.log("an error as occurred fetching the data");
       }
@@ -26,35 +20,23 @@ export default function Home() {
     fetchData();
   }, []);
 
-  // COINGECKO
-  // useEffect(async () => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await axios.get(
-  //         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  //       );
-  //       console.log(res.data[4].id);
-  //     } catch (error) {
-  //       console.log("an error as occurred fetching the data");
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
   return (
     <div className="flex flex-col w-full items-center justify-center min-h-screen py-2">
-      <h1 className="text-5xl">Rick and Morty API</h1>
-      <h3 className="text-2xl">Character List</h3>
-      <p className="text-base">more stuff</p>
+      <h1 className="text-5xl">Coin List</h1>
+      <h3 className="text-2xl">CoinGecko API</h3>
 
       <div className="flex flex-wrap w-10/12 justify-center items-center h-full overflow-x-scroll rounded-xl bg-gradient-to-br from-purple-200  to-blue-200">
-        {characterData.map((character) => (
-          <div key={character.id} className="flex m-1">
+        {coinData.map((coin) => (
+          <div key={coin.id} className="flex m-1">
             <CharacterCard
-              name={character.name}
-              gender={character.gender}
-              status={character.status}
-              image={character.image}
+              coinName={coin.name}
+              coinSymbol={coin.symbol}
+              coinID={coin.id}
+              coinImage={coin.image}
+              coinPriceChange24Hr={coin.price_change_percentage_24h}
+              coinMarketCap={coin.market_cap}
+              coinHigh24hr={coin.high_24h}
+              coinLow24Hr={coin.low_24h}
             />
           </div>
         ))}
