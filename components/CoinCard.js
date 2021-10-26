08d5dab7f363d43b3653b86e5b4bdd43bb496e7e
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-10-26 16:18:09 -0400

    added pagination

--- components/CoinCard.js
+++ components/CoinCard.js
@@ -11,22 +11,26 @@ function CoinCard({
   coinLow24Hr,
 }) {
   return (
-    <div className="flex flex-col w-64 bg-gradient-to-tr from-gray-200 to-white h-96 rounded-lg">
-      <div className="flex flex-col place-items-start pl-3 pt-2">
-        <h1>Name: {coinName}</h1>
-        <p>Symbol: {coinSymbol}</p>
-        <p>ID: {coinID}</p>
-        <p>Price Change %: {coinPriceChange24Hr}</p>
-        <p>Market Cap: {coinMarketCap}</p>
-        <p>24H High: {coinHigh24hr}</p>
-        <p>24H Low: {coinLow24Hr}</p>
-      </div>
-
-      <div className="flex w-full justify-center mt-4">
+    <div className="flex flex-row h-14 w-full bg-gradient-to-tr from-gray-200 to-white  rounded-lg">
+      <div className="flex h-full items-center justify-center pl-3">
         <img
-          className=" flex w-44 h-44 rounded-b-lg object-contain"
+          className=" flex w-10 h-10 rounded-lg object-contain"
           src={coinImage}
         ></img>
+      </div>
+      <div className="flex w-full justify-between items-center px-3">
+        <div className="flex flex-col h-full justify-center flex-start">
+          <h1>{coinName}</h1>
+          <p>{coinSymbol}</p>
+        </div>
+        <div className="flex flex-col place-items-start  h-full items-center">
+          <p>Price Change %: {coinPriceChange24Hr}</p>
+          <p>Market Cap: {coinMarketCap}</p>
+        </div>
+        <div className="flex flex-col h-full items-center">
+          <p>24H High: {coinHigh24hr}</p>
+          <p>24H Low: {coinLow24Hr}</p>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,85 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import CoinCard from "../components/CoinCard";
 
 export default function Home() {
   const [coinData, setCoinData] = useState([]);
-  const coinEndpoint =
+  const [pageNumber, setPageNumber] = useState(0);
+  const coinsPerPage = 10;
+  const pagesVisited = pageNumber * coinsPerPage;
+
+  const apiEndpoint =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
-  useEffect(async () => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(coinEndpoint);
-        setCoinData(res.data);
-      } catch (error) {
-        console.log("A problem has occured while trying to fetch data");
-      }
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(apiEndpoint);
+      const { data } = res;
+      setCoinData(data);
     };
-    fetchData();
+    getData();
   }, []);
 
-  return (
-    <div className="flex flex-col w-full items-center justify-center min-h-screen py-2">
-      <h1 className="text-5xl">Coin List</h1>
-      <h3 className="text-2xl">CoinGecko API</h3>
+  const displayCoins = coinData
+    .slice(pagesVisited, pagesVisited + coinsPerPage)
+    .map((coin) => {
+      return (
+        <div className={"flex w-full p-2"} key={coin.id}>
+          <CoinCard
+            coinName={coin.name}
+            coinSymbol={coin.symbol}
+            coinID={coin.id}
+            coinImage={coin.image}
+            coinPriceChange24Hr={coin.price_change_percentage_24h}
+            coinMarketCap={coin.market_cap}
+            coinHigh24hr={coin.high_24h}
+            coinLow24Hr={coin.low_24h}
+          />
+        </div>
+      );
+    });
 
-      <div className="flex flex-wrap w-10/12 justify-center items-center h-full rounded-xl bg-gradient-to-br from-purple-200 to-blue-200">
-        {coinData.map((coin) => (
-          <div key={coin.id} className="flex m-2">
-            <CoinCard
-              coinName={coin.name}
-              coinSymbol={coin.symbol}
-              coinID={coin.id}
-              coinImage={coin.image}
-              coinPriceChange24Hr={coin.price_change_percentage_24h}
-              coinMarketCap={coin.market_cap}
-              coinHigh24hr={coin.high_24h}
-              coinLow24Hr={coin.low_24h}
-            />
+  const pageCount = Math.ceil(coinData.length / coinsPerPage);
+
+  const pageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  return (
+    <div className="flex flex-col w-full items-center justify-between min-h-screen py-2">
+      <div className="flex flex-col w-full h-full items-center mt-10">
+        <h1 className="text-5xl">Coin List</h1>
+        <h3 className="text-2xl">CoinGecko API</h3>
+        <div className="flex w-10/12 justify-center items-center h-full rounded-xl bg-gradient-to-br from-purple-200 to-blue-200">
+          <div className="flex flex-col w-full items-center justify-center">
+            <div className="flex flex-col w-full items-center justify-center p-4">
+              {displayCoins}
+            </div>
           </div>
-        ))}
+        </div>
+      </div>
+      <div className="relative bottom-0 mt-5 mb-5">
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={pageChange}
+          pageRangeDisplayed={1}
+          marginPagesDisplayed={1}
+          containerClassName={
+            "flex w-96 justify-evenly bg-gray-700 h-12 rounded-full text-gray-400 items-center"
+          }
+          previousLinkClassName={
+            "flex items-center justify-center px-2 duration-200 hover:text-white "
+          }
+          nextLinkClassName={
+            "flex items-center justify-center px-2 duration-200 hover:text-white "
+          }
+          disabledClassName={"flex"}
+          activeLinkClassName={
+            "flex w-8 h-8 rounded-full items-center justify-center border-blue-400 border-2 text-white"
+          }
+        />
       </div>
     </div>
   );
